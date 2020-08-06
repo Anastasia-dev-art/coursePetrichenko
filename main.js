b135ec514e6b0f33165acf075accbaa2cb2a88a0
Anastasia <example@ru>
2020-08-06 20:16:47 +0300

    appStart

---
+++ main.js
@@ -0,0 +1,23 @@
+
+
+let numberOfFilm = +prompt ( "Сколько фильмов вы уже посмотрели?", " ")
+
+
+
+personalMovieDB = {
+    count: numberOfFilm,
+    movies : { },
+    actors: { },
+    genres:  [ ],
+    privat: false
+}
+
+let a = prompt("Один из последних фильмов, который вы посмотрели", " ");
+let b = prompt('Оценка фильму', " ");
+let c = prompt("Один из последних фильмов, который вы посмотрели", " ");
+let d = prompt('Оценка фильму', " ")
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies.c = d
+
+console.log(personalMovieDB);
